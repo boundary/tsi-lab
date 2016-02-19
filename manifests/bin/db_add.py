088d6f08044c463ad94fbe77ad822fabf225e632
Gwartney <davidg@boundary.com>
2016-02-19 14:51:05 -0800

    script to add data to database

--- manifests/bin/db_add.py
+++ manifests/bin/db_add.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
-
 import pymysql
-connection = pymysql.connect(user='admin', password='admin123', database='app')
-table = etl.fromdb(connection, 'SELECT * FROM business_metrics')
-print(table.look())
-sorted = etl.sort(table, 'dt');
-print(sorted.look())
+import os
+
+host = os.environ['DB_HOST']
+host = os.environ['DB_USER']
+host = os.environ['DB_PASSWORD']
+host = os.environ['DB_DATABASE']
+
+connection = pymysql.connect(host='localhost',
+                             user='admin',
+                             password='admin123',
+                             db='app',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "CALL insert_data({0})".format(1)
+        cursor.execute(sql)
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+finally:
+    connection.close()
