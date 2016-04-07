af8b224834212ac2cddc12dc3fbbf9d2af9b4d6b
Gwartney <davidg@boundary.com>
2016-04-07 13:19:44 -0700

    Stubbed out file

---
+++ manifests/bin/lab-2-events.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Import the API class from the tspapi package
+# Package can be installed using pip:
+#
+# $ pip install tspapi
+#
+from tspapi import API
+
+# Create an instance of the API
+api = API()
+
+# Create a new event 
+api.create_event(title="My Event", source=)
