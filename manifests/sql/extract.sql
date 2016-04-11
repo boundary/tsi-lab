6fca761f27127cbfa2b5688f4ad69285f7fdc8ae
Gwartney <davidg@boundary.com>
2016-04-11 10:23:08 -0700

    example SQL file

---
+++ manifests/sql/extract.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `email` varchar(255) COLLATE utf8_bin NOT NULL,
+    `password` varchar(255) COLLATE utf8_bin NOT NULL,
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT
+CHARSET=utf8
+COLLATE=utf8_bin
+AUTO_INCREMENT=1 ;
